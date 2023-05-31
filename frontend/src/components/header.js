cff0792758f415d7b6d2dc812bfe65c32416f19b
Bruna Dias <brunandradias@gmail.com>
2023-05-31 11:32:02 -0300

    cria tela principal e linka pop up de insercao

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,14 +1,16 @@
 import "./header.css"
 
 import logo from '../img/logo.svg'
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 function Header(){
+    let navigate = useNavigate(); 
+
     return(
     <div>
         <div className="header_container">
-            <img onClick={()=> redirect("/main")} className="header_logo" src={logo}></img>
+            <img onClick={() => navigate("/main")} className="header_logo" src={logo}></img>
         </div>
     </div>
     )
--- frontend/src/components/popUp/insertTransaction.js
+++ frontend/src/components/popUp/insertTransaction.js
@@ -1,34 +1,80 @@
 import "./insertTransaction.css";
 import "./globalPopUp.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import api from "../../services/api"
 
 
-function InsertTransaction(){
-    const[select, setSelect] = useState("despesa")
+function InsertTransaction({ display, setDisplay, type, setType }){
+    const [valor, setValor] = useState("");
+    const [data, setData] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [fontePagamento, setFontePagamento] = useState("")
+    const [categoria, setCategoria] = useState("")
+
+    const criaTransacao = ({
+        valor,
+        data,
+        descricao,
+        fontePagamento,
+        categoria
+    }) => {
+        api.post('/transaction', {
+            user_id: JSON.parse(localStorage.user).user_id,
+            valor,
+            data,
+            descricao,
+            fontePagamento,
+            categoria
+        }).then((res) => {
+            console.error(res);
+            window.location.reload();
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+
     return(
-        <div className="grid pop_up_container">
-            <select className="insert_transaction_select" value={select} onChange={(e) => setSelect(e.target.value)}>
-                <option value="despesa">Despesa</option>
-                <option value="receita">Receita</option>
-            </select>
+        <div className="block insert_transaction_modal" style={{ 'display': display }}>
+            <div className="flex insert_transaction_center">
 
-            <div className="grid insert_transaction_input_group">
-                <input className="pop_up_input" placeholder="R$0.00"></input>
-                <input className="pop_up_input" placeholder="Data"></input>
-                <input className="pop_up_input" placeholder="Descrição"></input>
-                <div className="grid pop_up_input_group_line
+                <div className="grid pop_up_container" >
+                    <select className="insert_transaction_select" value={type} 
+                        onChange={(e) => setType(e.target.value)}>
+                        <option value="despesa">Despesa</option>
+                        <option value="receita">Receita</option>
+                    </select>
 
-">
-                    <input className="pop_up_input" placeholder="Fonte de Pagamento"></input>
-                    <input className="pop_up_input" placeholder="Categoria"></input>
+                    <div className="grid insert_transaction_input_group">
+                        <input className="pop_up_input" placeholder="R$0.00" 
+                            onChange={(e)=>{setValor(e.target.value)}}/>
+                        <input className="pop_up_input" placeholder="Data"
+                            onChange={(e)=>{setData(e.target.value)}}/>
+                        <input className="pop_up_input" placeholder="Descrição"
+                            onChange={(e)=>{setDescricao(e.target.value)}}/>
+                        <div className="grid pop_up_input_group_line
+
+        ">
+                            <input className="pop_up_input" placeholder="Fonte de Pagamento"
+                                onChange={(e)=>{setFontePagamento(e.target.value)}}/>
+                            <input className="pop_up_input" placeholder="Categoria"
+                                onChange={(e)=>{setCategoria(e.target.value)}}/>
+                        </div>
+                    </div>
+                    <div className="pop_up_button_group">
+                        <button className="botao_voltar" onClick={() => setDisplay('none')}>Voltar</button>
+                        <button className="botao_confirmar" onClick={()=>criaTransacao(
+                            valor,
+                            data,
+                            descricao,
+                            fontePagamento,
+                            categoria
+                        )}>Criar</button>
+                    </div>
+
                 </div>
             </div>
-            <div className="pop_up_button_group">
-                <button className="botao_voltar">Voltar</button>
-                <button className="botao_confirmar">Criar</button>
-            </div>
-
         </div>
+        
     )
 }
 
