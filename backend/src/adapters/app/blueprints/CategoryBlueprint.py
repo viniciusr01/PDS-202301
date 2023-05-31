6eebffbfca6a48e18965a458aaab62d29cbf778d
eullersaez <eullersaez@gmail.com>
2023-05-31 15:10:04 -0300

    including a get method for category

--- backend/src/adapters/app/blueprints/CategoryBlueprint.py
+++ backend/src/adapters/app/blueprints/CategoryBlueprint.py
@@ -2,6 +2,8 @@ from flask import Blueprint, current_app, jsonify, request
 from src.domain.services.MakeCategory import MakeCategory
 from src.adapters.SqlAdapter import SqlAdapter
 from src.domain.gates.CategoryFactory import CategoryFactory
+from src.domain.services.RetrieveCategories import RetrieveCategories
+from src.domain.gates.dto.RetrieveCategoriesDTO import RetrieveCategoriesDTO
 
 
 category = Blueprint('category', __name__,)
@@ -25,3 +27,20 @@ def AddCategory():
     except Exception as e:
         print(e.with_traceback, e.args)
         return "An internal error occurred.", 500
+
+@category.route('/<user_id>', methods = ['GET'])
+def RetrieveCategory(user_id: int):
+    try:
+        category = RetrieveCategories(SqlAdapter()).make(user_id=str(user_id))
+
+        res = RetrieveCategoriesDTO().make(category)
+        
+        return jsonify(res)
+
+    except TypeError as e:
+        return jsonify(str(e)), 400
+         
+
+    except Exception as e:
+        print(e.args, e.with_traceback)
+        return "An internal error occurred. Please, try again later.", 500
