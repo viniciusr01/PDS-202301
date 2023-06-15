e8ec6aa1f63c81086e4e1632afc881bba7e18267
Leonardo Borges de Oliveira <beolorges@gmail.com>
2023-06-15 16:25:09 -0300

    fix quebra account

--- backend/src/domain/entities/Account.py
+++ backend/src/domain/entities/Account.py
@@ -2,9 +2,9 @@ from src.domain.value_objects.DefaultColor import DefaultColor
 
 class Account:
     def __init__(self, 
+                 id: int,
                  name: str, 
                  description: str, 
-                 id: int  = 0, 
                  fees: float = 0,
                  color: str = DefaultColor.code, 
                  balance: float = 0.0,
