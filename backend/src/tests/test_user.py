b42eea03fb1a068ace2416aafb34f9248dc9e050
eullersaez <eullersaez@gmail.com>
2023-06-21 02:01:49 -0300

    added auth test to user

--- backend/src/tests/test_user.py
+++ backend/src/tests/test_user.py
@@ -1,7 +1,24 @@
 from src.domain.entities.User import User
+from src.domain.entities.BankAccount import BankAccount
+from src.domain.services.Authentication import Authentication
+from src.domain.services.RetrieveUserAccounts import RetrieveUserAccounts
+from src.domain.gates.dto.RetrieveUserAccountDTO import RetrieveUserAccountDTO
+from src.adapters.SqlAdapter import SqlAdapter
+import pytest
 
 def test_create_user() -> None:
     user = User('11111111111', 'Test', 'test@test.com')
     assert user.cpf == '11111111111'
     assert user.name == 'Test'
     assert user.email == 'test@test.com'
+
+def test_make_authentication(monkeypatch: pytest.MonkeyPatch) -> None:
+    account = BankAccount(id='1', name='Conta Corrente', description='Conta Corrente', id_bank='1')
+    monkeypatch.setattr(SqlAdapter, "GetUser", lambda x, y: False)
+    monkeypatch.setattr(SqlAdapter, "CreateUser", lambda x, y, z, w: True)
+    monkeypatch.setattr(RetrieveUserAccounts, "make", lambda x, y: [account])
+    auth = Authentication(SqlAdapter()).make("11111111111", "Test", "test@test.com") 
+    assert auth['User']['name'] == 'Test'
+    assert auth['User']['email'] == 'test@test.com'
+    
+
