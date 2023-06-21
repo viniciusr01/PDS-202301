c070c2ee56935cc2e707ae099b67444be3f98287
eullersaez <eullersaez@gmail.com>
2023-06-21 03:20:13 -0300

    added some expense tests

--- backend/src/tests/test_expense.py
+++ backend/src/tests/test_expense.py
@@ -1,5 +1,10 @@
+from src.domain.entities.CreditCard import CreditCard
 from src.domain.entities.Expense import Expense
+from src.domain.gates.dto.RetrieveExpensesDTO import RetrieveExpensesDTO
+from src.domain.services.RetrieveExpensesInAPeriod import RetrieveExpensesInAPeriod
+from src.adapters.SqlAdapter import SqlAdapter
 from datetime import date
+import pytest
 
 def test_create_expense() -> None:
     expense = Expense('Test description', 100, '2022-07-07', 1)
@@ -7,3 +12,14 @@ def test_create_expense() -> None:
     assert expense.value == 100
     assert expense.reference_date == date(2022,7,7)
     assert expense.id_category == 1
+
+def test_retrieve_expenses_in_a_period(monkeypatch: pytest.MonkeyPatch) -> None:
+    expense = Expense('Test description', 100, '2022-07-07', 1)
+    credit_card = CreditCard('1', ' Test', 'Test description', 0, 'F7BC0A', 0)
+
+    monkeypatch.setattr(SqlAdapter, "RetrieveCreditCardsFromUser", lambda x, y, z: [credit_card])
+    monkeypatch.setattr(SqlAdapter, "RetrieveAccountsFromUser", lambda x, y: [])
+    monkeypatch.setattr(SqlAdapter, "RetrieveExpensesFromAccount", lambda x, y, z, w: [expense])
+
+    expenses = RetrieveExpensesInAPeriod(SqlAdapter()).make("11111111111", date(2022,7,7))
+    assert len(expenses) == 1
